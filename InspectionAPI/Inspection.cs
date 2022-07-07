6f8b33a16901ca8c125a357b1b9f56f996d30d61
Javier Paez <javierignacio.paez@gmail.com>
2022-07-07 15:59:11 -0300

    first commit

---
+++ InspectionAPI/Inspection.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InspectionAPI
+{
+    public class Inspection
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Status { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string Comment { get; set; } = string.Empty;
+
+
+        public int InspectionTypeId { get; set; }
+        public InspectionType? InspectionType { get; set; }
+    }
+}
---
+++ InspectionAPI/InspectionType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class InspectionType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
